686af689146d6f50b7de7764cd122d73a87c57d5
Lindorm236 <elielbjean@gmail.com>
2023-09-20 18:52:34 +0000

    init

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+//Création d'un tableau contenant les informations d'une équipe
+$team_1_data=["LesCanadiens", 3];
+echo $team_1_data[0];
+
+$team_1_data=[
+    "name"=>"Les Canadiens",
+    "score"=>3,
+];
+?>
